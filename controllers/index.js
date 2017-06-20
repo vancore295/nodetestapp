e8ea808b75feb9a2e73c55ca729e075414228ade
vancore295 <silverfire295@gmail.com>
2017-06-20 17:35:10 -0400

    moved editmenu function to own controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 (function(controllers) {
     var maincontroller = require("./maincontroller");
     var menucontroller = require("./menucontroller");
+    var editmenucontroller = require("./editmenucontroller");
+
     controllers.init = function(app) {
         maincontroller.init(app);
         menucontroller.init(app);
+        editmenucontroller.init(app);
     }
 })(module.exports);
