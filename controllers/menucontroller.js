58cd01cd40a39c29f0532ccc2e8466c30fb9acc1
vancore295 <silverfire295@gmail.com>
2017-06-20 16:40:40 -0400

    created a route that gets data from the database and then renders it in the view

--- controllers/menucontroller.js
+++ controllers/menucontroller.js
@@ -4,10 +4,17 @@
 
     menucontroller.init = function(app) {
         app.get("/menu", function(req, res) {
-            res.render("menu", { titlle: "Menu Test" });
-        })
+            res.render("menu", { title: "Menu Test" });
+        });
+        app.get("/editmenu", function(req, res) {
+            menuItem.find({}, function(err, menuitems) {
+                items = menuitems;
+                //console.log(menuitems)
+                res.render("editmenu", { title: "End menu test", data: menuitems });
+            });
+
+        });
         app.post("/addMenuItem", function(req, res) {
-            console.log(req.body);
             var newMenuItem = new menuItem({
                 route: req.body.route,
                 href: req.body.href,
@@ -22,9 +29,23 @@
             });
             newMenuItem.save();
             res.status(200).redirect("/menu");
-            //console.log(newMenuItem);
-
         });
-    }
+        app.post("/editMenuItem", function(req, res) {
+            var updatedMenuItem = new menuItem({
+                route: req.body.route,
+                href: req.body.href,
+                target: req.body.target,
+                linkLabel: req.body.linkLabel,
+                imageSource: req.body.imageSource,
+                dataToggle: req.body.dataToggle,
+                dataPlacement: req.body.dataPlacement,
+                title: req.body.title,
+                showImage: req.body.showImage || null,
+                showLabel: req.body.showLabel || null
+            });
+            //updatedMenuItem.update({_id: req.body._id},);
+            res.status(200).redirect("/editmenu");
+        });
+    };
 
-})(module.exports)
+})(module.exports);
